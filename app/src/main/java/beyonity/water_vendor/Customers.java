fd25dc0b2291c4ce331c61afc7dce2dc6ecc749e
RK <a.r.ramkumar25@gmail.com>
2017-08-30 20:41:29 +0530

    commit

--- app/src/main/java/beyonity/water_vendor/Customers.java
+++ app/src/main/java/beyonity/water_vendor/Customers.java
@@ -2,12 +2,55 @@ package beyonity.water_vendor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import beyonity.water_vendor.Adapter.Notification_Adapter;
 
 public class Customers extends AppCompatActivity {
-
+    private List<Notification> notificationList = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private Notification_Adapter mAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customers);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        mAdapter = new Notification_Adapter(notificationList);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mAdapter);
+
+        prepareNotificationData();
+    }
+    private void prepareNotificationData() {
+        Notification notif = new Notification("Ram", "7358339907", "2017", "Wed", "3", "15:00", "Kodungaiyur");
+        notificationList.add(notif);
+
+        notif = new Notification("Ram", "7358339907", "2017", "Fri", "5", "14:00","2nd street");
+        notificationList.add(notif);
+
+        notif = new Notification("Mohan", "7358339907", "2017", "Sat", "2", "13:00","1st street");
+        notificationList.add(notif);
+
+        notif = new Notification("Kumar", "7358339907", "2017", "Sun", "5", "12:00","madras");
+        notificationList.add(notif);
+
+        notif = new Notification("Ravi", "7358339907", "2017", "Wed", "1", "11:00","chennai");
+        notificationList.add(notif);
+
+        notif = new Notification("Chandran", "7358339907", "2017", "Wed", "9", "10:00","lol");
+        notificationList.add(notif);
+
+
+        mAdapter.notifyDataSetChanged();
     }
 }
