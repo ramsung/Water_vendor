3128ca143643a2e3684b54d6fdd92d4fc9eb5f5f
RK <a.r.ramkumar25@gmail.com>
2017-08-30 17:28:18 +0530

    commit

--- app/src/main/java/beyonity/water_vendor/MainActivity.java
+++ app/src/main/java/beyonity/water_vendor/MainActivity.java
@@ -7,14 +7,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.TextView;
-import android.content.Context;
-import android.view.GestureDetector;
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.Toast;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -52,8 +47,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-       // setSupportActionBar(toolbar);
 
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
 
