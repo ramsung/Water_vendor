65352b0662ee7ae834f28a4abfc51ee3387411e8
RK <a.r.ramkumar25@gmail.com>
2017-08-30 22:09:06 +0530

    commit

---
+++ app/src/main/java/beyonity/water_vendor/acchist.java
@@ -0,0 +1,13 @@
+package beyonity.water_vendor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class acchist extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acchist);
+    }
+}
